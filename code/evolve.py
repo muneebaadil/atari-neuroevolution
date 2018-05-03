08a564655f2549bfbe139a1abffa6f1b4e6e29d4
Muneeb Aadil <imuneebaadil@gmail.com>
2018-05-03 19:55:40 +0500

    confined the code style to ruler

--- code/evolve.py
+++ code/evolve.py
@@ -17,46 +17,69 @@ def GetParser():
     parser = argparse.ArgumentParser(description='Atari using Neuroevolution')
     
     #neural network parameters
-    parser.add_argument('--input_dim',action='store', type=str, default='210x160x3', dest='input_dim')
-    parser.add_argument('--num_hidden',action='store', type=int, default=6, dest='num_hidden')
-    parser.add_argument('--num_actions',action='store', type=int, default=6, dest='num_actions')
-    parser.add_argument('--init_func',action='store', type=str, default='gauss', dest='init_func')
-    parser.add_argument('--init_args',action='store', type=str, default='mu=0,sigma=1', dest='init_args')
+    parser.add_argument('--input_dim',action='store', type=str, default='210x160x3',
+                         dest='input_dim')
+    parser.add_argument('--num_hidden',action='store', type=int, default=6, 
+                        dest='num_hidden')
+    parser.add_argument('--num_actions',action='store', type=int, default=6,
+                        dest='num_actions')
+    parser.add_argument('--init_func',action='store', type=str, default='gauss',
+                         dest='init_func')
+    parser.add_argument('--init_args',action='store', type=str, default='mu=0,sigma=1',
+                         dest='init_args')
 
     #input/preprocess arguments
-    parser.add_argument('--preprocess',action='store', type=str, default='FramesDiff', dest='preprocess')
-    parser.add_argument('--preprocess_args',action='store', type=str, default='scale=255.', dest='preprocess_args')
+    parser.add_argument('--preprocess',action='store', type=str, default='FramesDiff',
+                         dest='preprocess')
+    parser.add_argument('--preprocess_args',action='store', type=str, 
+                        default='scale=255.', dest='preprocess_args')
 
     #optimization parameters
-    parser.add_argument('--population_size',action='store',type=int,default=1, dest='population_size')
+    parser.add_argument('--population_size',action='store',type=int,default=1,
+                         dest='population_size')
     parser.add_argument('--num_gens',action='store',type=int,default=10, dest='num_gens')
     
-    parser.add_argument('--crossover_type',action='store',type=str,default='cxTwoPoint', dest='crossover_type')
-    parser.add_argument('--crossover_prob', action='store', type=float, default=.9, dest='crossover_prob')
+    parser.add_argument('--crossover_type',action='store',type=str,default='cxTwoPoint',
+                         dest='crossover_type')
+    parser.add_argument('--crossover_prob', action='store', type=float, default=.9,
+                         dest='crossover_prob')
 
-    parser.add_argument('--mutate_type', action='store', type=str, default='mutGaussian', dest='mutate_type')
-    parser.add_argument('--mutate_args',action='store',type=str,default='mu=1,sigma=1', dest='mutate_args')
-    parser.add_argument('--mutate_prob',action='store',type=float,default=.1, dest='mutate_prob')
-    parser.add_argument('--mutate_indpb',action='store',type=float,default=.5, dest='mutate_indpb')
+    parser.add_argument('--mutate_type', action='store', type=str, default='mutGaussian',
+                         dest='mutate_type')
+    parser.add_argument('--mutate_args',action='store',type=str,default='mu=1,sigma=1',
+                         dest='mutate_args')
+    parser.add_argument('--mutate_prob',action='store',type=float,default=.1, 
+                        dest='mutate_prob')
+    parser.add_argument('--mutate_indpb',action='store',type=float,default=.5, 
+                        dest='mutate_indpb')
 
-    parser.add_argument('--select_type',action='store', type=str, default='selTournament', dest='select_type')
-    parser.add_argument('--num_select',action='store', type=int, default=2, dest='num_select')
-    parser.add_argument('--select_args',action='store', type=str, default='tournsize=2', dest='select_args')
+    parser.add_argument('--select_type',action='store', type=str,
+                         default='selTournament', dest='select_type')
+    parser.add_argument('--num_select',action='store', type=int, default=2,
+                         dest='num_select')
+    parser.add_argument('--select_args',action='store', type=str, default='tournsize=2',
+                         dest='select_args')
     
     #evaluation parameters
-    parser.add_argument('--game_name',action='store', type=str, default='Pong-v0', dest='game_name')
-    parser.add_argument('--num_episodes',action='store', type=int, default=1, dest='num_episodes')
+    parser.add_argument('--game_name',action='store', type=str, default='Pong-v0',
+                         dest='game_name')
+    parser.add_argument('--num_episodes',action='store', type=int, default=1, 
+                        dest='num_episodes')
 
     #logging/verbosity parameters.. 
     parser.add_argument('--load',action='store', type=str, default=None, dest='load')
-    parser.add_argument('--exp_root_dir',action='store', type=str, default='../experiments', dest='exp_root_dir')
-    parser.add_argument('--exp_name',action='store', type=str, default=strftime("%Y-%m-%d__%H-%M-%S",gmtime()),
-                         dest='exp_name')
-    parser.add_argument('--save_every',action='store', type=int, default=-1, dest='save_every')
-    parser.add_argument('--hof_maxsize',action='store', type=int, default=1, dest='hof_maxsize')
+    parser.add_argument('--exp_root_dir',action='store', type=str, 
+                        default='../experiments', dest='exp_root_dir')
+    parser.add_argument('--exp_name',action='store', type=str, 
+                        default=strftime("%Y-%m-%d__%H-%M-%S",gmtime()),dest='exp_name')
+    parser.add_argument('--save_every',action='store', type=int, default=-1,
+                         dest='save_every')
+    parser.add_argument('--hof_maxsize',action='store', type=int, default=1, 
+                        dest='hof_maxsize')
     
     #misc
-    parser.add_argument('--render',action='store', type=bool, default=False, dest='render')
+    parser.add_argument('--render',action='store', type=bool, default=False, 
+                        dest='render')
     
     return parser
 
@@ -68,10 +91,14 @@ def PostprocessOpts(opts):
     opts.num_biases = reduce(add, opts.dims[1:], 0)
     opts.num_params = opts.num_weights + opts.num_biases
 
-    opts.mutate_args = {x.split('=')[0]: int(x.split('=')[1]) for x in opts.mutate_args.split(',')}
-    opts.select_args = {x.split('=')[0]: int(x.split('=')[1]) for x in opts.select_args.split(',')}
-    opts.init_args = {x.split('=')[0]: float(x.split('=')[1]) for x in opts.init_args.split(',')}
-    opts.preprocess_args = {x.split('=')[0]: float(x.split('=')[1]) for x in opts.preprocess_args.split(',')}
+    opts.mutate_args = {x.split('=')[0]: int(x.split('=')[1]) \
+                        for x in opts.mutate_args.split(',')}
+    opts.select_args = {x.split('=')[0]: int(x.split('=')[1]) \
+                        for x in opts.select_args.split(',')}
+    opts.init_args = {x.split('=')[0]: float(x.split('=')[1]) \
+                        for x in opts.init_args.split(',')}
+    opts.preprocess_args = {x.split('=')[0]: float(x.split('=')[1])\
+                             for x in opts.preprocess_args.split(',')}
 
     opts.exp_dir = os.path.join(opts.exp_root_dir, opts.exp_name)
     return 
@@ -92,13 +119,14 @@ def InitSetup(opts):
     toolbox.register("neuron_init", getattr(random, opts.init_func), **opts.init_args)
     toolbox.register("network_init", tools.initRepeat, creator.Network, 
                                 toolbox.neuron_init, n=opts.num_params)
-    toolbox.register("population_init", tools.initRepeat, list, toolbox.network_init, n=opts.population_size)
+    toolbox.register("population_init", tools.initRepeat, list, toolbox.network_init,
+                     n=opts.population_size)
 
     #cross-over, mutation and selection strategy.. 
     toolbox.register("evaluate", Evaluate, opts=opts)
     toolbox.register("mate", getattr(tools, opts.crossover_type))
-    toolbox.register("mutate", getattr(tools, opts.mutate_type), indpb = opts.mutate_indpb, 
-                    **opts.mutate_args)
+    toolbox.register("mutate", getattr(tools, opts.mutate_type),
+                     indpb = opts.mutate_indpb, **opts.mutate_args)
     toolbox.register("select", getattr(tools, opts.select_type))
 
     #configuring logging
@@ -154,7 +182,8 @@ def Evolve(opts):
             os.makedirs(os.path.join(exp_dir,'checkpoints'))
         
         filename = os.path.join(exp_dir,'checkpoints','ckpt_{}.pkl'.format(num_ckpts))
-        to_save = {'pop':pop, 'hof':hof,'gen':gen, 'logbook':logbook, 'randstate':random.getstate()}
+        to_save = {'pop':pop, 'hof':hof,'gen':gen, 'logbook':logbook, 
+                    'randstate':random.getstate()}
 
         with open(filename,'w') as ckpt_file:
             pickle.dump(to_save, ckpt_file)
