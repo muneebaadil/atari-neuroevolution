f38d2a40f56c44aad656352d5d6d6070df69f817
Muneeb Aadil <imuneebaadil@gmail.com>
2018-05-04 04:22:05 +0500

    saving only HOF while checkpointing

--- code/evolve.py
+++ code/evolve.py
@@ -184,17 +184,18 @@ def Evolve(opts):
         print_str += ', time taken = {}'.format(time()-last_time)
         print print_str
 
-    def _SaveCkpts(exp_dir, num_ckpts, gen, pop, hof, logbook): 
+    def _SaveCkpts(exp_dir, num_ckpts, hof): 
 
         if not os.path.exists(os.path.join(exp_dir,'checkpoints')): 
             os.makedirs(os.path.join(exp_dir,'checkpoints'))
         
-        filename = os.path.join(exp_dir,'checkpoints','ckpt_{}.pkl'.format(num_ckpts))
-        to_save = {'pop':pop, 'hof':hof,'gen':gen, 'logbook':logbook, 
-                    'randstate':random.getstate()}
+        ckpt_dir = os.path.join(exp_dir,'checkpoints')
+        if not os.path.exists(ckpt_dir): 
+            os.makedirs(os.path.join(ckpt_dir))
 
+        filename = os.path.join(ckpt_dir, 'hof_{}'.format(num_ckpts))
         with open(filename,'w') as ckpt_file:
-            pickle.dump(to_save, ckpt_file)
+            pickle.dump(hof, ckpt_file)
 
     #initial setup and population..
     creator, toolbox, stats, logbook, hof, pop, start_gen = InitSetup(opts)
@@ -215,7 +216,7 @@ def Evolve(opts):
         #optional checkpointing
         if (opts.save_every > 0) and (curr_gen % opts.save_every == 0): 
             num_ckpts += 1 
-            _SaveCkpts(opts.exp_dir, num_ckpts, curr_gen, pop, hof, logbook)
+            _SaveCkpts(opts.exp_dir, num_ckpts, hof)
 
             #plotting
             PlotLog(logbook, opts.game_name, os.path.join(opts.exp_dir, 'plot.png'))
